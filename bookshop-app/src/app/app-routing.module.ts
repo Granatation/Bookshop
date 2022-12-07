0755d7c0888b38cdd8ae1253e60039452e4f624e
Nikola Emilov <nikico248@gmail.com>
2022-12-07 16:26:10 +0200

    login form

--- bookshop-app/src/app/app-routing.module.ts
+++ bookshop-app/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { HomeComponent } from './core/home/home.component';
 
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   }
 ];
 
--- bookshop-app/src/app/auth/login/login.component.ts
+++ bookshop-app/src/app/auth/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { appEmailValidator, sameValueGroupValidator } from 'src/app/shared/validators';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +10,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginForm = this.fb.group({
+    username: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, appEmailValidator]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  })
 
+  constructor(private fb: FormBuilder, private router: Router) {
+
+  }
+
+  registerHandler() {
+    this.router.navigate(['/']);
+  }
 }
