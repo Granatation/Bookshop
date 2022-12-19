3b770827867f6f025644e135b446e036c3fad93f
Nikola Emilov <nikico248@gmail.com>
2022-12-19 17:45:31 +0200

    update

--- bookshop-app/server/routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const bookController = require('./controllers/bookController');
-const userController = require('./controllers/userController');
-
-router.use(bookController);
-router.use(userController);
-
-module.exports = router;
--- server/config/env.js
+++ server/config/env.js
@@ -1,4 +1,4 @@
 exports.PORT = 3030;
-exports.DB_QUERYSTRING = 'mongodb+srv://Granatation:n24n8n10n6@atlascluster.rx6su9v.mongodb.net';
+exports.DB_QUERYSTRING = 'mongodb+srv://Granatation:n24n8n10n6@atlascluster.rx6su9v.mongodb.net/test';
 exports.SALT_ROUNDS = 10;
 exports.SECRET = 'tercesemos'
